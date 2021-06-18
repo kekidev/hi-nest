bf9ac5de0b834055822cbe3460ac05daafbf86c8
keki <baejaehwan1125@gmail.com>
2021-06-18 14:34:08 +0900

    final

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -6,7 +6,11 @@ import {
   Param,
   Patch,
   Post,
+  Req,
+  Res,
 } from '@nestjs/common';
+import { CreateMovieDTO } from './DataTransferObject/create-movie.dto';
+import { UpdateMovieDTO } from './DataTransferObject/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -21,22 +25,22 @@ export class MoviesController {
   }
 
   @Get('/:id')
-  getOne(@Param('id') movieId: string): Movie {
+  getOne(@Param('id') movieId: number): Movie {
     return this.moviesService.getOne(movieId);
   }
 
   @Post()
-  create(@Body() movieData) {
+  create(@Body() movieData: CreateMovieDTO) {
     return this.moviesService.create(movieData);
   }
 
   @Delete('/:id')
-  remove(@Param('id') movieId: string) {
+  remove(@Param('id') movieId: number) {
     return this.moviesService.deleteOne(movieId);
   }
 
   @Patch('/:id')
-  patch(@Param('id') movieId: string) {
-    return `This will patch a movie with the id: ${movieId}`;
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDTO) {
+    return this.moviesService.update(movieId, updateData);
   }
 }
